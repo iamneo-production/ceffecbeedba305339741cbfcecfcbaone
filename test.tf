ab435f37c038aaee641d8a185ed3d516f8b0ea36
Iamneo <techadmin@iamneo.ai>
2023-09-22 20:46:05 +0000

    Backup Commit

---
+++ test.tf
@@ -0,0 +1,12 @@
+terraform{
+    required_providers {
+      aws = {
+          source = "hashicorp/aws"
+      }
+    }    
+}
+provider "aws"{
+    region = "ap-northeast-1"
+    access_key = "AKIAWWT6ZO2AGOILD27F"
+    secret_key = "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"
+}
